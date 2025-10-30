77dd1f1dbc6e59e956702506b889857da50ecb02
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2025-10-30 15:55:04 -0300

    Atualização

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Vamos Navegar nos aprendizados</h1>
+    <?php 
+        echo "Ola mundo PHP";
+    ?>
+</body>
+</html>
